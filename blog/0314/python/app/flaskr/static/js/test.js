001a5ab7fce2679538149cad7ce1fd8f755df012
konchangakita <satoru.kondou@nutanix.com>
2022-03-11 23:16:17 +0900

    0314

---
+++ blog/0314/python/app/flaskr/static/js/test.js
@@ -0,0 +1,25 @@
+const element =
+  <div className="container">
+    <h1>Welcome to UUID X-plorer</h1>
+    <div className="content ">
+      <form submit="/" method="post" className="float_l" >
+        <input type="text" name="prism_ip" placeholder="Cluster IP" size="12" />
+        <input type="text" name="prism_user" placeholder="Uesrname" size="10" />
+        <input type="password" name="prism_pass" placeholder="Password" size="10" />
+        <input type="hidden" name="connect" value="true" />
+        <input type="submit" value="データ収集" />
+      </form>
+      <form submit="/" method="post">
+        <input type="text" name="cluster_name" placeholder="cluster_name" size="10" ></input>
+        <input type="hidden" name="display" value="true" />
+        <input type="submit" value="表示" />
+      </form>
+    </div>
+    <div className="list">
+    </div>
+  </div>
+
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);

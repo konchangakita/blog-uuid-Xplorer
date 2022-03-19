694f4dcea5251c879a833d88e5953b22b81d0eea
kon-chan <>
2022-03-20 02:33:32 +0900

    0321

---
+++ blog/0321/python/app/flaskr/app.py
@@ -0,0 +1,103 @@
+from flask import Flask
+from flask import render_template, request
+import time
+import json
+
+import data_broker
+
+app = Flask(__name__)
+
+# set data_broker class
+ntnx = data_broker.NutanixAPI()
+ELASTIC_SERVER = 'http://elasticsearch:9200'
+es = data_broker.ElasticAPI(es_server=ELASTIC_SERVER)
+
+title =  'Welcome to UUID X-plorer'
+
+def create_essmple(res_list):
+    for _data in res_list:
+        data = res_list[_data].json()
+        path = 'sample/' + _data + '.json'
+        with open(path, 'w') as f:
+            f.write(json.dumps(data))
+
+def connect_cluster():
+    cluster_name = ''
+    # Formから受け取り
+    prism_ip = request.form.get('prism_ip')
+    prism_user = request.form.get('prism_user')
+    prism_pass = request.form.get('prism_pass')
+
+    # get from Nutanix cluster
+    res_list = ntnx.get_xdata(prism_ip, prism_user, prism_pass)
+
+    # sample
+    #create_essmple(res_list)
+
+    if hasattr(res_list['vms'], 'status_code'):
+        if res_list['vms'].status_code == 200:
+            # input to Elasticsearch
+            cluster_name, input_size = es.input_data(res_list)
+            time.sleep(1)
+            content = "データ取得完了"
+        else:
+            r_json = res_list['vms'].json()
+            content = r_json['message_list'][0]['message']
+    else: # timeout
+        content = 'timeout (wrong IP?)'
+
+    return content, cluster_name
+
+def get_dataset(cluster_name):
+    timeslot = es.get_timeslot(cluster_name)
+    timestamp_utcstr = timeslot[0]['utc_time']
+    hits = es.get_document_all(timestamp_utcstr, cluster_name)
+
+    doc = {}
+    for _hit in hits:
+        index_name = _hit['_index']
+        if not index_name in doc:
+            doc[index_name] = []
+
+        doc[index_name].append(_hit['_source'])
+
+    return doc
+
+@app.route('/')
+def index():
+    return render_template('index.html', \
+        title = title)
+
+@app.route('/', methods=['POST'])
+def index_post():
+    content = ''
+    if request.form.get('connect'):
+        content, cluster_name = connect_cluster()
+    elif request.form.get('display'):
+        cluster_name = request.form.get('cluster_name')
+        content = get_dataset(cluster_name)
+
+    return render_template('index.html', \
+        content = content, \
+        cluster_name = cluster_name, \
+        title = title)
+
+
+if __name__  == '__main__':
+    app.run(host="0.0.0.0", port=777, debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ blog/0321/python/app/flaskr/static/js/test.js
@@ -0,0 +1,109 @@
+class List extends React.Component {
+  render () {
+    return (
+      <div className="list">
+      </div>
+    );
+  }
+}
+
+class FormDisplay extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cluster_name: ''
+    };
+    this.handleChangeCluster = this.handleChangeCluster.bind(this);
+  }
+
+  handleChangeCluster(event) {
+    console.log(event.target.name, event.target.value)
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  render () {
+    return (
+      <form submit="/" method="post">
+        <input type="text" name="cluster_name" placeholder="Cluster name" size="10" value={this.state.prism_ip} onChange={this.handleChangeCluster} ></input>
+        <input type="hidden" name="display" value="true" />
+        <input type="submit" value="表示" />
+      </form>
+    );
+  }
+}
+
+class FormConnect extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      prism_ip: '',
+      prism_user: '',
+      prism_pass: ''
+    };
+    this.handleChangePrism = this.handleChangePrism.bind(this);
+  }
+
+  handleChangePrism(event) {
+    console.log(event.target.name, event.target.value)
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  render () {
+    return (
+      <form className="float_l" >
+        <input type="text" name="prism_ip" placeholder="Cluster IP" size="12" value={this.state.prism_ip} onChange={this.handleChangePrism} />
+        <input type="text" name="prism_user" placeholder="Uesrname" size="10" value={this.state.prism_user} onChange={this.handleChangePrism} />
+        <input type="password" name="prism_pass" placeholder="Password" size="10" value={this.state.prism_pass} onChange={this.handleChangePrism} />
+        <input type="hidden" name="connect" value="true" />
+        <input type="submit" value="データ収集" />
+      </form>
+    );
+  }
+}
+
+class Form extends React.Component {
+  render () {
+    return (
+      <div>
+        <FormConnect />
+        <FormDisplay />
+      </div>
+    );
+  }
+}
+
+class Content extends React.Component {
+  render () {
+    return (
+      <div className="content ">
+        <Form />
+        <List />
+      </div>
+    );
+  }
+}
+
+class Container extends React.Component {
+  render () {
+    return (
+      <div className="container">
+        <h1>Welcome to UUID X-plorer</h1>
+        <Content />
+      </div>
+    );
+  }
+}
+
+
+ReactDOM.render(
+  <Container />,
+  document.getElementById('root')
+);
